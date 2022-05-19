eafa0a462696270e8fbd4514d9d2479d9c2a7b69
EC2 Default User <ec2-user@ip-172-31-1-185.ec2.internal>
2022-05-19 08:59:25 +0000

    bookers-40

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+
   def index
     @book = Book.new
     @books = Book.all
@@ -23,8 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: "You have updated book successfully."
     else
       @books = Book.find(params[:id])
       render :edit
@@ -35,8 +37,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id),notice: "You have created book successfully."
     else
       @books = Book.all
       @user = current_user
@@ -54,5 +55,11 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def top
+  flash[:notice] = "Sign out successfully."
   end
 
   def about
